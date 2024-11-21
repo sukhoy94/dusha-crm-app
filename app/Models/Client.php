65b1b1fe725ddef1dc3b09da79a89423cdc57565
andriisukhoi <andrii.sukhoi@gmail.com>
2024-11-21 22:45:47 +0100

    Remove a child when client is deleted

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\RelationshipType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -29,5 +30,15 @@ class Client extends Model
     public function children(): HasMany
     {
         return $this->hasMany(FamilyRelation::class, 'guardian_id');
+    }
+
+    public static function booted(): void
+    {
+        static::deleting(function (Client $client) {
+            $client->children->each(function (FamilyRelation $relation) {
+                $relation->child->delete();
+                $relation->delete();
+            });
+        });
     }
 }
