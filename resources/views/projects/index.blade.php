df8dbbad01da45e41d9fb2b3d79ff769151dfabf
andriisukhoi <andrii.sukhoi@gmail.com>
2024-12-26 15:12:40 +0100

    Wyświetlenie listy projektow z bazy

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -33,16 +33,17 @@
             </tr>
             </thead>
             <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Warsztaty artystyczne cykliczne
-                </th>
-                <td class="flex items-center px-6 py-4">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Szczegóły</a>
-                    <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Usuń</a>
-                </td>
-            </tr>
-
+            @foreach($projects as $project)
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $project->title }}
+                    </th>
+                    <td class="flex items-center px-6 py-4">
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Szczegóły</a>
+                        <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Usuń</a>
+                    </td>
+                </tr>
+            @endforeach
             </tbody>
         </table>
     </div>
